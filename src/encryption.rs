991bdaf7c602ae0fc3c125fab8b282ee5fe5875e
таня <taniaonyshkevych@gmail.com>
2024-03-15 23:20:31 +0200

    Added encryption function

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,4 +1,24 @@
+use std::fs;
+use std::io;
+use crate::files;
+
 pub fn encryption_hello()
 {
     println!("Hello, From encryption.rs")
 }
+
+fn encrypt_char(ch: char) -> char {
+    ((ch as u8) + 1) as char
+}
+
+fn encrypt_string(s: &str) -> String {
+    s.chars().map(encrypt_char).collect()
+}
+
+
+fn main() -> std::io::Result<()> {
+    let file_contents = files::read_files("D://SPZProject//src//test.txt")?;
+
+    Ok(())
+}
+
