67cd2d79e1af8c338ec6392f9bbb7c2e7fd1b29a
таня <taniaonyshkevych@gmail.com>
2024-03-15 23:28:01 +0200

    Test

--- src/encryption.rs
+++ src/encryption.rs
@@ -2,3 +2,15 @@ pub fn encryption_hello()
 {
     println!("Hello, From encryption.rs")
 }
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn test()
+    {
+        encryption_hello()
+    }
+}
