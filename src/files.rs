e4359863f4de81f8e11f8dbc5ab14dbf9e3fce47
володя <eesucksc@gmail.com>
2024-03-15 22:49:56 +0200

    Added and Tested 'read_files' function

--- src/files.rs
+++ src/files.rs
@@ -1,14 +1,18 @@
-use walkdir::WalkDir;
 use std::io;
+use std::fs;
+use std::collections::HashMap;
+
+use walkdir::WalkDir;
+
 
 pub fn files_hello()
 {
     println!("Hello, From files.rs")
 }
 
-pub fn print_files(folder_path: &str) -> io::Result<()>
+pub fn print_files(folders_path: &str) -> io::Result<()>
 {
-    for file_entry in WalkDir::new(folder_path)
+    for file_entry in WalkDir::new(folders_path)
     {
         let entry = match file_entry {
             Ok(entry) => { entry }
@@ -26,6 +30,41 @@ pub fn print_files(folder_path: &str) -> io::Result<()>
     Ok(())
 }
 
+pub fn read_files(folders_path: &str) -> io::Result<HashMap<String, String>>
+{
+    let mut result: HashMap<String, String> = HashMap::new();
+
+    for file_entry in WalkDir::new(folders_path)
+    {
+        let entry = match file_entry {
+            Ok(entry) => { entry }
+            Err(error) =>
+                {
+                    println!("[ERROR] {}", error);
+
+                    continue;
+                }
+        };
+
+        let file_path = entry.path();
+        let file_content= match fs::read_to_string(file_path)
+        {
+            Ok(content) => { content }
+            Err(error) =>
+                {
+                    println!("[ERROR] {}", error);
+
+                    continue;
+                }
+        };
+
+        let file_path_string = String::from(file_path.to_str().unwrap());
+        result.insert(file_path_string, file_content);
+    }
+
+    Ok(result)
+}
+
 #[cfg(test)]
 mod tests
 {
@@ -37,5 +76,20 @@ mod tests
         let folders_path: &str = "Assets\\TestFiles";
 
         let _ = print_files(folders_path);
+    }
+
+    #[test]
+    fn test_reading() -> io::Result<()>
+    {
+        let folders_path: &str = "Assets\\TestFiles";
+
+        let result = read_files(folders_path)?;
+
+        for (key, value) in result
+        {
+            println!("{}: {}", key, value);
+        }
+
+        Ok(())
     }
 }
