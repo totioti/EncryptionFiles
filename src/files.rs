5ddab969849cc212d2782d1c8e1af1ca65516948
володя <eesucksc@gmail.com>
2024-03-15 22:21:59 +0200

    Added and Tested 'print_files' function

--- src/files.rs
+++ src/files.rs
@@ -1,4 +1,41 @@
+use walkdir::WalkDir;
+use std::io;
+
 pub fn files_hello()
 {
     println!("Hello, From files.rs")
+}
+
+pub fn print_files(folder_path: &str) -> io::Result<()>
+{
+    for file_entry in WalkDir::new(folder_path)
+    {
+        let entry = match file_entry {
+            Ok(entry) => { entry }
+            Err(error) =>
+                {
+                    println!("[ERROR] {}", error);
+
+                    continue;
+                }
+        };
+
+        println!("{}", entry.path().display());
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn test_print_files()
+    {
+        let folders_path: &str = "Assets\\TestFiles";
+
+        let _ = print_files(folders_path);
+    }
 }
